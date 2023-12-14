63fbc2528b5eed7bc0e8f256d16630ed81c13df5
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-14 01:58:07 +0100

    created the various components for the website

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Services from './Services'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+import SpecialOffer from './SpecialOffer'
+
+export {
+    Hero,
+    Services,
+    SuperQuality,
+    Footer,
+    PopularProducts,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffer
+}
