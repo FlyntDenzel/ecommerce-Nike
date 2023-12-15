f277970ce4941eec6f20477a7d56742576a57900
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-15 10:43:42 +0100

    created basic navigation bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import {headerLogo} from '../assets/images'
 import {hamburger} from '../assets/icons'
+import {navLinks} from '../constants/index'
 
 const Nav = () => {
   return (
@@ -9,7 +10,16 @@ const Nav = () => {
                 <img src={headerLogo} alt="logo" width={130} height={29} />
             </a>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+              {/* using paranthesis instead of curly brackets to return it immediately */}
+              {navLinks.map((item) => (
+                  <li key={item.label}>
+                    <a href={item.href} className=' font-montserrat leading-normal text-lg text-slate-gray'>{item.label}</a>
+                  </li>
+              ))}
             </ul>
+            <div className='hidden max-lg:block'>
+              <img src={hamburger} alt="hamburger" width={25} height={25}/>
+            </div>
         </nav>
     </header>
   )
