a65dac59272b3442b9766dc7862667c9a24dc109
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 05:36:48 +0100

    adding elements to the footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,27 @@
+import { footerLogo } from "../assets/images"
+import { socialMedia } from "../constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="footerLogo" width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base leading-7  font-montserrat text-white-400 sm:max-w-sm">Get Shoes ready for the new term at your Nike Store. 
+          Find your perfect Size in Store. Get Rewards</p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full" key={icon.src}>
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </footer>
   )
 }
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -6,7 +6,7 @@ const Subscribe = () => {
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign up for
         <span className=" text-coral-red"> Updates </span> & NewsLetter
       </h3>
-      <div className=" lg:max-w-[40%] w-full">
+      <div className=" lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
            <Button label="Sign Up"/>
