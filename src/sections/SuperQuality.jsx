1b5040ff2c8d445d7523ac3dc659591afc8a42ba
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 04:38:37 +0100

    created image and content of the specialOffer  section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -6,7 +6,6 @@ const SuperQuality = () => {
   return (
    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
     <div className="flex flex-1 flex-col">
-    {/* <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p> */}
         <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
           We provide you
           <span className=" text-coral-red"> Super</span>
