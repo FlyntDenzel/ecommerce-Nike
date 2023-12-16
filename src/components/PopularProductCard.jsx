dd5a414ede8d2e8eef84d062082d83040eb8ee95
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 03:50:36 +0100

    applied style to the popular products images

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -6,7 +6,10 @@ const PopularProductCard = ({imgURL, name, price}) => {
         <img src={imgURL} alt={name} className="w-[280] h-[280]" />
         <div className="mt-8 flex justify-start gap-2.5">
             <img src={star} alt="rating" width={24} height={24} />
+            <p className=" font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
         </div>
+        <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+        <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
     </div>
   )
 }
