77894365a7f23dc9219338c2229bae3348791ef6
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 03:42:48 +0100

    added a star/rating to the product

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,7 +1,12 @@
+import { star } from "../assets/icons"
+
 const PopularProductCard = ({imgURL, name, price}) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
         <img src={imgURL} alt={name} className="w-[280] h-[280]" />
+        <div className="mt-8 flex justify-start gap-2.5">
+            <img src={star} alt="rating" width={24} height={24} />
+        </div>
     </div>
   )
 }
