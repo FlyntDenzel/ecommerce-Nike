bd2b5e41e71c7d040a17feee50e65dbaca12362e
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 03:02:39 +0100

    added images next to the image preview

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,24 @@
-const ShoeCard = () => {
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
+    const handleClick = () => {
+        if (bigShoeImage !== imgURL.bigShoe) {
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    }
+
   return (
-    <div>ShoeCard</div>
+    <div 
+    className={`border-2 rounded-xl
+        ${bigShoeImage === imgURL
+            ? 'border-coral-red'
+            : 'border-transparent'
+         } cursor-pointer max-sm:flex-1
+    `}
+         onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card br-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+         <img src={imgURL.thumbnail} alt="shoe collection" width={127} height={103} className="object-contain" />
+      </div>
+    </div>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,11 @@ import {arrowRight} from "../assets/icons/index"
 import {shoes, statistics} from "../constants/index"
 import { bigShoe1 } from "../assets/images"
 import ShoeCard from "../components/ShoeCard"
+import { useState } from "react"
 
 const Hero = () => {
+  const [image, setImage] = useState()
+
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
@@ -29,10 +32,14 @@ const Hero = () => {
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary  bg-cover bg-center">
         <img src={bigShoe1} alt="shoe collection" width={610} height={500} className="object-contain relative z-10" />
 
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
             {shoes.map((shoe, index) => (
               <div key={index}>
-                <ShoeCard />
+                <ShoeCard  
+                  imgURL={shoe}
+                  changeBigShoeImage={() => {}}
+                  bigShoeImage =""
+                  />
               </div>
             ))}
         </div>
