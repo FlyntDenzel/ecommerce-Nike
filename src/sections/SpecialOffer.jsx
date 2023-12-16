22265af0b13d0a297947aa1392613bbc1948e7ba
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 04:52:55 +0100

    customized button from the specialoffer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -16,8 +16,9 @@ const SpecialOffer = () => {
          From premier selections to incredible savings, we offer unparalled value that sets us apart.</p>
         <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectaions.
          Your journey with us is nothing  short of exceptional</p>
-        <div className="mt-11">
+        <div className="mt-11 flex flex-wrap gap-4">
           <Button label="Shop Now" iconURL={arrowRight}/>
+          <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
         </div>
     </div>
     </section>
