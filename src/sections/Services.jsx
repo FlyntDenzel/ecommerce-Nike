522770b0a73f64c4717e01d3c1fbdd2ad37e64bd
FlyntDenzel <flyntdenzel2004@gmail.com>
2023-12-16 04:27:17 +0100

    added icons to each component

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service) =>(
-        <ServiceCard />
+        <ServiceCard key={service.label} {...service} />
       ))}
     </section>
   )
